f4455aa29a17641ba32aec17492a9065d36a1da5
voccelstudio <voccelstudio@gmail.com>
2026-04-03 17:37:33 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './news_dashboard.jsx' // <--- CLAVE: Que el nombre sea igual al archivo
+import App from './news_dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
